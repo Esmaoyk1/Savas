cc42d17d64e05e8ea866c03b3453e05f150c5ffc
esmanur oyanık <esmaoyanik55@gmail.com>
2025-05-29 12:17:25 +0300

    Form tasarımı yapıldı

--- Savas.Desktop/AnaForm.cs
+++ Savas.Desktop/AnaForm.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace Savas.Desktop
 {
-    public partial class Form1 : Form
+    public partial class AnaForm : Form
     {
-        public Form1()
+        public AnaForm()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
